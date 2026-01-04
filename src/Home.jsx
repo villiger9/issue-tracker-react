2696085ca5d75663bd9a6704915f56881208c69d
villiger9 <ali.shaker93@gmail.com>
2026-01-04 22:07:21 +0300

    added error logic to home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,19 +3,32 @@ import IssueList from "./IssueList";
 
 const Home = () => {
   const [issues, setIssues] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/issues")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setIssues(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading</div>}
       {issues && <IssueList issues={issues} title="all issues" />}
     </div>
   );
