6da05fd30069419705abaa6a3e877ea80214d0d2
villiger9 <ali.shaker93@gmail.com>
2026-01-05 22:10:46 +0300

    added RootLayout and router. Deleted Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import Navbar from "./Navbar";
 import Home from "./Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />}></Route>
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+/* import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from "react-router-dom";
+
+// 1. Define the router outside the component
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}> {// RootLayout contains your Navbar and Outlet }
+      <Route index element={<Home />} />
+    </Route>
+  )
+); 
+
+function App() {
+  // 2. Use RouterProvider instead of <Router>
+  return <RouterProvider router={router} />;
+} */
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,21 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div className="layout">
+      <header>
+        <nav className="navbar">
+          <h1>The issue tracker</h1>
+          <NavLink to="/" className="links">
+            Home
+          </NavLink>
+          <NavLink to="create">New Issue</NavLink>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
