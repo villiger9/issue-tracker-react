8298a9489325261e3e8c70884b7fe7fdf3a4e354
villiger9 <ali.shaker93@gmail.com>
2026-01-27 23:08:08 +0300

    fixed context / implemented dark mode

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -1,13 +1,8 @@
 import { Link } from "react-router-dom";
-import { ThemeContext } from "../src/contexts/ThemeContext"; // Import the 'Station'
-import { useContext } from "react";
 
 const IssueList = ({ issues, title }) => {
-  const { toggleTheme } = useContext(ThemeContext);
-
   return (
     <div className="issue-list">
-      <button onClick={toggleTheme}>Toggle Dark Mode</button>
       <h2>{title}</h2>
       {issues.map((issue) => (
         <div className="issue-preview" key={issue.id}>
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,10 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { useContext } from "react";
 
 export default function RootLayout() {
+  const { toggleTheme } = useContext(ThemeContext);
+
   return (
     <div className="layout">
       <header>
@@ -12,6 +16,7 @@ export default function RootLayout() {
           <NavLink to="create" className="links">
             New Issue
           </NavLink>
+          <button onClick={toggleTheme}>Toggle Dark Mode</button>
         </nav>
       </header>
 
